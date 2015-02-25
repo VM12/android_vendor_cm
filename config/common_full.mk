b56a163c332a31544a01ec4f9c0c6d71ca3476a7
XsMagical <XsMagical@team-nocturnal.net>
2015-02-25 16:31:37 -0500

    Removed Live Wallpapers
    Who uses this anymore? 
    
        HoloSpiralWallpaper \
        LiveWallpapers \
        LiveWallpapersPicker \
        MagicSmokeWallpapers \

--- config/common_full.mk
+++ config/common_full.mk
@@ -10,10 +10,6 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/cm/overlay/dictionaries
 # Optional CM packages
 PRODUCT_PACKAGES += \
     Galaxy4 \
-    HoloSpiralWallpaper \
-    LiveWallpapers \
-    LiveWallpapersPicker \
-    MagicSmokeWallpapers \
     NoiseField \
     PhaseBeam \
     VisualizationWallpapers \
