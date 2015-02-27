8937792ca1d9dd6664686f202b0e343fc43b05cd
XsMagical <XsMagical@team-nocturnal.net>
2015-02-26 23:26:58 -0500

    updated path

--- config/common_full.mk
+++ config/common_full.mk
@@ -1,8 +1,8 @@
 # Inherit common CM stuff
-$(call inherit-product, vendor/cm/config/common.mk)
+$(call inherit-product, vendor/vm12/config/common.mk)
 
 # Include CM audio files
-include vendor/cm/config/cm_audio.mk
+include vendor/vm12/config/cm_audio.mk
 
 # Include CM LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/cm/overlay/dictionaries
