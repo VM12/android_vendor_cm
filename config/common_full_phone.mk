40a54eb8f9cf7bd6ae407793580ae0e697c14cd1
XsMagical <XsMagical@team-nocturnal.net>
2015-02-26 23:27:18 -0500

    Update common_full_phone.mk

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,5 +1,5 @@
 # Inherit common CM stuff
-$(call inherit-product, vendor/cm/config/common_full.mk)
+$(call inherit-product, vendor/vm12/config/common_full.mk)
 
 # Default notification/alarm sounds
 PRODUCT_PROPERTY_OVERRIDES += \
