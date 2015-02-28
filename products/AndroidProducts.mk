cc21b20d927c95aad90135116c13e58610bf9ec7
GruesomeWolf <boober19841126@gmail.com>
2015-02-28 00:31:32 -0500

    fixup vins device

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,3 +1,4 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/bacon.mk \
+    $(LOCAL_DIR)/kltespr.mk \
     $(LOCAL_DIR)/shamu.mk 
---
+++ products/kltespr.mk
@@ -0,0 +1,7 @@
+
+$(call inherit-product, vendor/vm12/config/common_full_phone.mk)
+$(call inherit-product, vendor/vm12/config/nfc_enhanced.mk)
+$(call inherit-product, device/samsung/kltespr/full_kltespr.mk)
+PRODUCT_DEVICE := kltespr
+PRODUCT_NAME := vm12_kltespr
+
