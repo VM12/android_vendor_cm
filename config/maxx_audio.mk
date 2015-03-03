d20b166ae0a70ade26da43bb89fae1b611da4d28
GruesomeWolf <boober19841126@gmail.com>
2015-03-03 10:24:47 -0500

    My duh

--- config/maxx_audio.mk
+++ config/maxx_audio.mk
@@ -1,11 +1,11 @@
 # MaxxAudio FX for One+
 PRODUCT_COPY_FILES += \
-    vendor/vm12/prebuilt/maxx/default.mps:system/etc/waves \
-    vendor/vm12/prebuilt/maxx/MaxxAudioFX.apk:system/priv-app/MaxxAudio \
-    vendor/vm12/prebuilt/maxx/audio_effects.conf:system/vendor/etc \
-    vendor/vm12/prebuilt/maxx/libMA3-processcode-Coretex_A9.so:system/vendor/lib \
-    vendor/vm12/prebuilt/maxx/libMA3-processcode-Qualcomm.so:system/vendor/lib \
-    vendor/vm12/prebuilt/maxx/libmaxxeffect-cembedded.so:system/vendor/lib/soundfx \
-    vendor/vm12/prebuilt/maxx/libqcbassboost.so:system/vendor/lib/soundfx \
-    vendor/vm12/prebuilt/maxx/libqcreverb.so:system/vendor/lib/soundfx \
-    vendor/vm12/prebuilt/maxx/libqcvirt.so:system/vendor/lib/soundfx 
+    vendor/vm12/prebuilt/maxx/default.mps:system/etc/waves/default.mps \
+    vendor/vm12/prebuilt/maxx/MaxxAudioFX.apk:system/priv-app/MaxxAudio/MaxxAudioFX.apk \
+    vendor/vm12/prebuilt/maxx/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    vendor/vm12/prebuilt/maxx/libMA3-processcode-Coretex_A9.so:system/vendor/lib/libMA3-processcode-Coretex_A9.so \
+    vendor/vm12/prebuilt/maxx/libMA3-processcode-Qualcomm.so:system/vendor/lib/libMA3-processcode-Qualcomm.so \
+    vendor/vm12/prebuilt/maxx/libmaxxeffect-cembedded.so:system/vendor/lib/soundfx/libmaxxeffect-cembedded.so \
+    vendor/vm12/prebuilt/maxx/libqcbassboost.so:system/vendor/lib/soundfx/libqcbassboost.so \
+    vendor/vm12/prebuilt/maxx/libqcreverb.so:system/vendor/lib/soundfx/libqcreverb.so \
+    vendor/vm12/prebuilt/maxx/libqcvirt.so:system/vendor/lib/soundfx/libqcvirt.so 
